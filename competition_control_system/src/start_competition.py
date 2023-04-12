13879dc6a87ae44bb23bc763a657be70858a214a
koundinyavinnakota <koundinya.vinnakota@gmail.com>
2023-04-12 01:35:23 -0400

    Added sensor nodes

--- competition_control_system/src/start_competition.py
+++ competition_control_system/src/start_competition.py
@@ -8,6 +8,16 @@ def main(args=None):
 
     interface = CompetitionInterface()
     interface.start_competition()
+    while rclpy.ok():
+        try:
+            rclpy.spin_once(interface)
+            image = interface.camera_image
+            interface.get_logger().info(f'Part Count: {interface.part_count}', throttle_duration_sec=2.0)
+
+            if image is not None:
+                interface.get_logger().info(interface.parse_advanced_camera_image(image), throttle_duration_sec=5.0)
+        except KeyboardInterrupt:
+            break
     interface.destroy_node()
     rclpy.shutdown()
 
